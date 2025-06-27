8a8e0133441725cfb845b3108afe5f2abcf91275
alxjandrotsc1 <jalejandro.torresc01@outlook.com>
2025-06-26 20:49:33 -0600

    last-update

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import path, { resolve } from 'node:path';
+import * as glob from 'glob';
+
+const obtenerEntradas = () => {
+  return Object.fromEntries(
+    glob.sync('./**/*.html', {
+      ignore: ['./dist/**', './node_modules/**']
+    }).map(file => [
+      file.slice(0, file.length - path.extname(file).length),
+      resolve(__dirname, file)
+    ])
+  );
+};
+
+export default defineConfig({
+  appType: 'mpa',
+  base: process.env.DEPLOY_BASE_URL,
+  build: {
+    rollupOptions: {
+      input: obtenerEntradas()
+    },
+    minify: true
+  },
+});
+
